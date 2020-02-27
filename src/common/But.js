cdd311acd70271a34c5b76d1be680941c5c695df
Livingstone Asabahebwa <ahebwa49@gmail.com>
2020-02-27 10:37:53 +0300

    tweek the algorithm

---
+++ src/common/But.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const But = () => {
+  return (
+    <div>
+      <h1>But ...</h1>
+    </div>
+  );
+};
+
+export default But;
--- src/common/Progress.js
+++ src/common/Progress.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Filler = props => (
-  <div className="filler" style={{ width: `${props.percentage}%` }} />
+  <div className="filler" style={{ width: `${props.reverse}%` }} />
 );
 
 class Progress extends React.Component {
@@ -13,8 +13,15 @@ class Progress extends React.Component {
   render() {
     const { percentage, reverse } = this.props;
     return (
-      <div className="tracker">
-        <Filler percentage={percentage} reverse={reverse} />
+      <div className="progress">
+        <div className="progress-header">
+          <div>Healthy</div>
+          <div>Unhealthy</div>
+          <div>Abusive</div>
+        </div>
+        <div className="tracker">
+          <Filler percentage={percentage} reverse={reverse} />
+        </div>
       </div>
     );
   }
