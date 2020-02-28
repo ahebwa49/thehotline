ff79f2d9d7b27a7157d9ab19d7b2ffb6fc011a0b
Livingstone Asabahebwa <ahebwa49@gmail.com>
2020-02-28 09:44:53 +0300

    style the desktop popup

--- src/common/Progress.js
+++ src/common/Progress.js
@@ -1,8 +1,19 @@
 import React from "react";
 
-const Filler = props => (
-  <div className="filler" style={{ width: `${props.reverse}%` }} />
-);
+const Filler = props => {
+  const styles = {
+    filler: {
+      height: "100%",
+      width: `${props.reverse}%`,
+      background:
+        "linear-gradient(90deg, #a81010 0%, #ef934c 39.31%, #41a67f 100%)",
+      transform: "matrix(-1, 0, 0, 1, 0, 0)",
+      // background: "linear-gradient(90deg, green 0%, orange 100%)",
+      borderRadius: "10px"
+    }
+  };
+  return <div style={styles.filler} />;
+};
 
 class Progress extends React.Component {
   constructor(props) {
